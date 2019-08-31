5346bb6d0a17893456044d784570bd506d4e1d5e
quentin <ledilavrecquentin@gmail.com>
2019-08-31 10:22:02 +0200

    got files from m1_internship/star-instrumented-chrome

---
+++ something.js
@@ -0,0 +1,11 @@
+ // TODO find why this line avoid Snap
+function aaabbbccc(params) {
+	params;
+	console.log(9684);
+	return 768111;
+}
+function aaababbccc(params) {
+	if (params===3) aaabbbccc();
+	console.log(9684);
+	return 768111;
+}
