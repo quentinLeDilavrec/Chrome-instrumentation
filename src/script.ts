2b9892bd57332f93c45480cbe0f9e58cba6cbb25
quentin <ledilavrecquentin@gmail.com>
2019-10-21 16:07:08 +0200

    common path opti

--- src/script.ts
+++ src/script.ts
@@ -9,9 +9,9 @@ import { join } from "path";
 
 const babel_js_src = fs.readFileSync(join(__dirname, "../babel.js"), 'utf8')
 
-function _MO_instantiator(instrumenter_container_str: string,root_path:string="") {
+function _MO_instantiator(instrumenter_container_str: string,common_path:string="") {
   const binding = window['logger']
-  window["global"] = {};
+  window["global"] = window["global"] || {} as any;
 
   const replacer = function (depth = Number.MAX_SAFE_INTEGER) {
     let objects, stack, keys;
@@ -67,8 +67,10 @@ function _MO_instantiator(instrumenter_container_str: string,root_path:string=""
   let log = [];
   const count = 2000;
 
-  const myCallPrinter = (call) => {
-    return '' + call[0].slice(root_path.length) + (call.length > 1 ? ' ' + JSON.stringify(call.slice(1), replacer(0)) : '');
+  const myCallPrinter = common_path.length>0?(call) => {
+    return '' + call[0].slice(common_path.length) + (call.length > 1 ? ' ' + JSON.stringify(call.slice(1), replacer(0)) : '');
+  } : (call) => {
+    return '' + call[0] + (call.length > 1 ? ' ' + JSON.stringify(call.slice(1), replacer(0)) : '');
   };
 
   function flush() {
@@ -242,8 +244,8 @@ async function instrument_basic(page: puppeteer.Page) {
  * replace original response with interceptions added to the functions
  * @param page the page to instrument
  */
-async function instrument_fetch(root_path:string, page: puppeteer.Page, output: string, apply_babel = false) {
-  page.on("popup", new_page => instrument_fetch(root_path.substr(-1)==='/'?root_path:root_path+'/', new_page, output, apply_babel))
+async function instrument_fetch(common_path:string, page: puppeteer.Page, output: string, apply_babel = false) {
+  page.on("popup", new_page => instrument_fetch(common_path.substr(-1)==='/'?common_path:common_path+'/', new_page, output, apply_babel))
   const client = await page.target().createCDPSession();
 
   if (!fs.existsSync(output)) fs.mkdirSync(output);
@@ -274,7 +276,7 @@ async function instrument_fetch(root_path:string, page: puppeteer.Page, output: 
     }
   })
 
-  await page.evaluateOnNewDocument(_MO_instantiator, instrumenter_container.toString(),root_path)
+  await page.evaluateOnNewDocument(_MO_instantiator, instrumenter_container.toString(),common_path)
 
   if (apply_babel) {
     await client.send('Fetch.enable', { patterns: [{ resourceType: "Script", requestStage: "Response" }] })
@@ -302,7 +304,13 @@ async function instrument_fetch(root_path:string, page: puppeteer.Page, output: 
 }
 
 // Main
-export async function launchBrowser(root_path:string, start_page: string = 'about:blank', output?: string) {
+/**
+ * 
+ * @param common_path prefix to remove from calls path
+ * @param start_page first page loaded on the instrumented browser
+ * @param output where the trace should go, use an absolute path to avoid troubles
+ */
+export async function launchBrowser(common_path:string, start_page: string = 'about:blank', output?: string) {
   // instantiating browser
   const options = { headless: false, dumpio: true, pipe: false };
   const launch_params = process.argv[2] === '--no-sandbox' ? [...puppeteer.defaultArgs(options), '--no-sandbox', '--disable-setuid-sandbox'] : puppeteer.defaultArgs(options);
@@ -311,7 +319,7 @@ export async function launchBrowser(root_path:string, start_page: string = 'abou
   browser.on('disconnected', () => console.log('instrumented browser session finished'))
   // instantiating starting pages
   const [page] = await browser.pages()
-  await instrument_fetch(root_path, page,
+  await instrument_fetch(common_path, page,
     output
     || (
       console.log("no output directory given use default output directory '/tmp/behavior_traces/default_browser/'"),
